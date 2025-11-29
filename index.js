044f3e440817c9bd95bdc73fbb60fca3fadadcfd
Akash N N <akashnn2003@gmail.com>
2025-11-29 20:16:05 +0530

    Added js for Filering Men categories

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 let showProduct = document.querySelector(".show-products");
 
-
 async function all(){
     const res=await fetch("https://fakestoreapi.com/products")
     const jsonRes= await res.json()
+    console.log(jsonRes);
     
     showProduct.innerHTML=jsonRes.map(e=>`
-        <div class="card"><img src=${e.image}></div>
+        <div class="card">
+            <img src=${e.image}>
+            <h5>${e.title.slice(0,12).concat("...")}<h5>
+        
+        
+        
+        </div>
         
         `).join("")
     
 }
-
 all()
+
+// -------------------------------filter by category-----------
+
+async function FilterMen(){
+    const res=await fetch("https://fakestoreapi.com/products");
+    const Jsonres=await res.json();
+    console.log(Jsonres);
+    const MenCategory=Jsonres.filter((e)=>e.category=="men's clothing");
+    
+     showProduct.innerHTML=MenCategory.map(e=>`
+        <div class="card">
+            <img src=${e.image}>
+            <h5>${e.title.slice(0,12).concat("...")}<h5>
+
+        </div>
+        
+        `).join("")
+            
+        
+    
+}
+ 
